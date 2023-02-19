296da698b3be9fc6e9c1d916c4401392c394fa6c
Jamari Broussard <jamari@Jamaris-MacBook-Air.local>
2023-02-19 11:43:14 -0600

    first commit

---
+++ client/script.js
@@ -0,0 +1,141 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container'); 
+
+let loadInterval;
+
+
+function loader(element){
+ element.textContent =  ''
+
+ loadInterval = setInterval(() => {
+    // Update the text content of the loading indicator
+    element.textContent += '.';
+
+    //if the loading indicator has reached three dots, reset it
+    if(element.textContent === '....'){
+        element.textContent = '';
+    }
+ }, 300)
+}
+
+//This code is a function that takes two parameters, an element and a text. It then creates an interval that runs every 20 milliseconds. Inside the interval, it checks if the index is less than the length of the text. If it is, it adds the character at the current index to the innerHTML of the element and increments the index. If not, it clears the interval.
+function typeText(element, text){
+    let index = 0;
+//he charAt() method returns the character at a specified index (position) in a string.
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++;
+        } else {
+            clearInterval(interval);
+        }
+    }, 20)
+}
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+
+function generateUniqueId(){
+    const timeStamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timeStamp}-${hexadecimalString}`; 
+}
+
+/* This function is responsible for the color change from user to Ai to determine who is speaking */
+function chatStripe (isAi, value, uniqueId){
+    return (
+        `
+            <div class="wrapper ${isAi && 'ai'}">
+                <div class="chat">
+                    <div class="profile">
+                        <img 
+                            src="${isAi ? bot : user}"
+                            alt="${isAi ? 'bot' : 'user'}"
+                        />
+                    </div>
+                    <div class="message" id=${uniqueId}>${value}</div>
+                </div>
+            </div>
+        `
+    )
+}
+
+
+/* This handle function will be the trigger to get the Ai generated response */
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    //user's chat stripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+    // to clear the textarea input
+    form.reset();
+
+    //bot's chat stripe
+    const uniqueId = generateUniqueId();
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+    // to focus scroll to the bottom to continue seeing messages
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    //fetch a newly created div
+    const messageDiv = document.getElementById(uniqueId)
+
+    //this turns on the loader
+    loader(messageDiv);
+
+    //this area is where we can fetch data from server and get the bot's response
+
+    const response = await fetch('http://localhost:5000', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
+//after response, need to clear interval
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = "";
+// needs to be clear in order for us to add our message
+// await response.json gives us the actually response from the backend but we need to parse the data
+    if(response.ok){
+        const data = await response.json();
+        const parsedData = data.bot.trim();
+
+        console.log({parsedData})
+// next pass through typeText function
+        typeText(messageDiv, parsedData);
+    } else {
+        const err = await response.text();
+
+        messageDiv.innerHTML = "Something went wrong";
+
+        alert(err);
+    }
+}
+
+/*in order to see the changes in handlesubmit we need to hold
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13){
+       handleSubmit(e);
+    }
+})
+*/
+
+//allows you to press enter,
+form.addEventListener('submit', handleSubmit);
+document.querySelector('#submit-button').addEventListener('click', (e) => {
+    e.preventDefault();
+    handleSubmit(e);
+});
---
+++ server/server.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+//allows use of the dotenv variables
+dotenv.config();
+
+console.log(process.env.OPENAI_API_KEY)
+
+//configuration function that accepts an object
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+//this creates an instance of openai then passes in the configuration
+const openai = new OpenAIApi(configuration);
+
+//next step initalize express application, express is called as a function
+const app = express();
+
+//setting up a couple middlewares
+//this allows cross origin request and allow the server to be called from the frontend
+app.use(cors());
+
+//this allows json to be passed from the frontend to the backend
+app.use(express.json());
+
+//dummy root route
+//asynchronous function that is going to accept a request and response
+app.get('/', async(req, res) => {
+    res.status(200).send({
+        message: 'Hello from CodeX',
+    })
+})
+
+//app.get you can't receive alot of data from the frontend
+//app.post allows a body or payload
+//same as post on this time we can get the data from the body of the frontend request
+//first wrap everything in a try and catch block
+
+//const repsonse create pr get a response from the open API
+//.createCompletetion is a function that accepts an object
+
+
+//after copying the parameters from https://platform.openai.com/playground/p/default-openai-api?lang=node.js&model=text-davinci-003
+//erase prompt parameter and replace with `${prompt}` because it is the prompt from the frontend text area
+//temperature - higher temperature value means the model will take more risk, change to 0 because want to avoid risk,
+//so model will answer with what it knows
+//max tokens - is the maximum amount of tokens to generate in a completition, change to 3000 to give long responses
+//top frequency means how often will it give similiar sentences so set to 0.5
+app.post('/', async (req,res) => {
+    try{
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+            
+
+        });
+// once a response is given want to send it back to the frontend
+     res.status(200).send({
+        bot: response.data.choices[0].text
+     })
+    }catch (error) {
+        console.log(error);
+        res.status(500).send({ error });
+
+    }
+})
+
+//now to make sure the server always listens
+
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
